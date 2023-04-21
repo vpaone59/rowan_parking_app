77a37e0f1499784c58147e1a9f8b2cffac14bdcd
Cohen <cohenj@rowan.edu>
2023-04-21 17:33:57 -0400

    Initialize sample made from YT tutorial
    Following along with youTube tutorial
    
    Basic sample for reading in static image and mask, use image classifier on masked sections of image

---
+++ main.py
@@ -0,0 +1,34 @@
+import cv2 
+import matplotlib.pyplot as plt 
+import numpy as np 
+
+from util import get_parking_spots_bboxes, empty_or_not 
+
+# paths to images for mask and lot
+mask = './images/carpark_test_mask2.jpg'
+lot = './images/carpark_test.jpg'
+# read in images in grayscale
+mask = cv2.imread(mask, 0)
+lot = cv2.imread(lot)
+
+connected_components = cv2.connectedComponentsWithStats(mask, 16, cv2.CV_32S)
+spots = get_parking_spots_bboxes(connected_components)
+#@print(spots[0])
+
+for spot in spots:
+    x1, y1, w, h = spot 
+    # check to see what's in the area of interest 
+    spot_crop = lot[y1:y1 + h, x1:x1 + w, :]
+    # is spot empty or not
+    spot_stuff = empty_or_not(spot_crop)
+    if spot_stuff:
+        lot = cv2.rectangle(lot, (x1, y1), (x1 + w, y1 + h), (0, 255, 0), 2)
+    else:
+        lot = cv2.rectangle(lot, (x1, y1), (x1 + w, y1 + h), (0, 0, 255), 2)
+
+    # blue rectangle
+    #lot = cv2.rectangle(lot, (x1, y1), (x1 + w, y1 + h), (255, 0, 0), 2)
+
+cv2.imshow('lot', lot)
+cv2.imshow('mask', mask)
+cv2.waitKey(0)
---
+++ util.py
@@ -0,0 +1,98 @@
+# https://github.com/computervisioneng/parking-space-counter/blob/master/util.py
+#import pickle
+
+from skimage.transform import resize
+import numpy as np
+import cv2
+import time
+
+EMPTY = True
+NOT_EMPTY = False
+min_width, min_height = 4, 4
+# set model confidence level, at least 50% confident in model classification 
+confidence_threshold = 0.5
+nms_threshold = 0.25
+# even though this is probably safe I don't feel great opening a pickle file from the web
+#MODEL = pickle.load("model.p", "rb")
+
+# Setting up yolo model instead
+# Load names of classes and get random colors
+classes = open('../yolo_test/coco.names').read().strip().split('\n')
+np.random.seed(42)
+colors = np.random.randint(0, 255, size=(len(classes), 3), dtype='uint8')
+
+# Give the configuration and weight files for the model and load the network.
+net = cv2.dnn.readNetFromDarknet('../yolo_test/yolov3.cfg', '../yolo_test/yolov3.weights')
+net.setPreferableBackend(cv2.dnn.DNN_BACKEND_OPENCV)
+# determine the output layer
+ln = net.getLayerNames()
+print('layer: ', net.getUnconnectedOutLayers())
+ln = [ln[i - 1] for i in net.getUnconnectedOutLayers()]
+
+
+def empty_or_not(spot_bgr):
+
+    obj_test = '' 
+  
+    spot_bgr = spot_bgr.astype(np.float32)
+    blob = cv2.dnn.blobFromImage(spot_bgr, 1/255.0, (416, 416), swapRB=True, crop=False)
+
+    #r = blob[0, 0, :, :]
+    net.setInput(blob)
+    t0 = time.time()
+    outputs = net.forward(ln)
+    t = time.time()
+    # print(len(outputs))
+    # for out in outputs:
+    #     print(out.shape)
+
+    # TODO: fine tune and refine
+    boxes = []
+    confidences = []
+    classIDs = []
+    h, w = spot_bgr.shape[:2]
+    for output in outputs:
+        for detection in output:
+            scores = detection[5:]
+            classID = np.argmax(scores)
+            confidence = scores[classID]
+            if confidence > confidence_threshold:
+                box = detection[:4] * np.array([w, h, w, h])
+                (centerX, centerY, width, height) = box.astype("int")
+                x = int(centerX - (width / 2))
+                y = int(centerY - (height / 2))
+                box = [x, y, int(width), int(height)]
+                boxes.append(box)
+                confidences.append(float(confidence))
+                classIDs.append(classID)
+    indices = cv2.dnn.NMSBoxes(boxes, confidences, confidence_threshold, nms_threshold)
+    # taking this from yolo example and only looking for classes detected
+    if len(indices) > 0:
+        for i in indices.flatten():
+            (x, y) = (boxes[i][0], boxes[i][1])
+            (w, h) = (boxes[i][2], boxes[i][3])
+            obj_test = classes[classIDs[i]]
+            text = "{}: {:.4f}".format(classes[classIDs[i]], confidences[i])
+            print(text)
+
+    if obj_test == 'car' or obj_test == 'truck':
+        return NOT_EMPTY
+    else:
+        return EMPTY
+    
+def get_parking_spots_bboxes(connected_components):
+    (totalLabels, label_ids, values, centroid) = connected_components
+
+    slots = []
+    coef = 1
+    for i in range(1, totalLabels):
+        # get coords
+        x1 = int(values[i, cv2.CC_STAT_LEFT] * coef)
+        y1 = int(values[i, cv2.CC_STAT_TOP] * coef)
+        h = int(values[i, cv2.CC_STAT_HEIGHT] * coef)
+        w = int(values[i, cv2.CC_STAT_WIDTH] * coef)
+        # for some reason get some bounding boxes that are 1 to 4 pixels wide, check the width to skip adding them
+        if w < min_width or h < min_height:
+            continue       
+        slots.append([x1, y1, w, h])
+    return slots
