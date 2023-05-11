fe3108d311d7af97f8bd918c454557e3e2d7d93e
Cohen <cohenj@rowan.edu>
2023-05-11 18:06:27 -0400

    created Util class and modified some opencv functions
    basic utility helper class and added mask and lot image overlay

--- main.py
+++ main.py
@@ -3,19 +3,18 @@ PICTURES/IMAGES MUST BE RESIZED TO 1920x1080
 """
 
 
-
 import cv2 
 import matplotlib.pyplot as plt 
 import numpy as np 
 import os
 
-from util import get_parking_spots_bboxes, empty_or_not 
+from util import Util 
 
 for filename in os.listdir(os.getcwd() + '/images/masks'):
     # paths to images for mask and lot
     print(filename)
     
-    if filename.endswith('.jpg'): # CHANGE FILE EXTENSION BASED ON MASK USED
+    if filename.endswith('.jpg') : # CHANGE FILE EXTENSION BASED ON MASK USED
        
         mask = './images/masks/' + filename
         print(mask)
@@ -25,9 +24,9 @@ for filename in os.listdir(os.getcwd() + '/images/masks'):
         # read in images in grayscale
         mask = cv2.imread(mask, 0)
         lot = cv2.imread(lot)
-
+        utilHlper = Util()
         connected_components = cv2.connectedComponentsWithStats(mask, 16, cv2.CV_32S)
-        spots = get_parking_spots_bboxes(connected_components)
+        spots = utilHlper.get_parking_spots_bboxes(connected_components)
         #@print(spots[0])
 
         for spot in spots:
@@ -35,15 +34,27 @@ for filename in os.listdir(os.getcwd() + '/images/masks'):
             # check to see what's in the area of interest 
             spot_crop = lot[y1:y1 + h, x1:x1 + w, :]
             # is spot empty or not
-            spot_stuff = empty_or_not(spot_crop)
+            spot_stuff, spot_obj_name = utilHlper.empty_or_not(spot_crop)
+            spot_obj_name = spot_obj_name.split(":")[0]
             if spot_stuff:
                 lot = cv2.rectangle(lot, (x1, y1), (x1 + w, y1 + h), (0, 255, 0), 2)
+                cv2.putText(lot, spot_obj_name, (x1, y1 - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 55), 1)
+                #print(spot_obj_name)
             else:
                 lot = cv2.rectangle(lot, (x1, y1), (x1 + w, y1 + h), (0, 0, 255), 2)
+                cv2.putText(lot, spot_obj_name, (x1, y1 - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 55), 1)
+                #print(spot_obj_name)
 
             # blue rectangle
             #lot = cv2.rectangle(lot, (x1, y1), (x1 + w, y1 + h), (255, 0, 0), 2)
 
-        cv2.imshow('lot', lot)
-        cv2.imshow('mask', mask)
-        cv2.waitKey(0)
+        #cv2.imshow('lot_'+filename, lot)
+        #cv2.imshow('mask_'+filename, mask)
+        
+        # display masked image of spaces overlayed on input image of parking lot
+        alpha = 0.5
+        gray_lot = cv2.cvtColor(lot, cv2.COLOR_BGR2GRAY)
+        color_mask = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)
+        overlay = cv2.addWeighted(lot, alpha, color_mask, 1-alpha, 0)
+        cv2.imshow('Overlay_'+filename, overlay)
+cv2.waitKey(0)
--- util.py
+++ util.py
@@ -6,93 +6,100 @@ import numpy as np
 import cv2
 import time
 
-EMPTY = True
-NOT_EMPTY = False
-min_width, min_height = 4, 4
-# set model confidence level, at least 50% confident in model classification 
-confidence_threshold = 0.5
-nms_threshold = 0.25
-# even though this is probably safe I don't feel great opening a pickle file from the web
-#MODEL = pickle.load("model.p", "rb")
+class Util:
 
-# Setting up yolo model instead
-# Load names of classes and get random colors
-classes = open('./models/coco.names').read().strip().split('\n')
-np.random.seed(42)
-colors = np.random.randint(0, 255, size=(len(classes), 3), dtype='uint8')
+    def __init__(self):
+            self.EMPTY = True
+            self.NOT_EMPTY = False
 
-# Give the configuration and weight files for the model and load the network.
-net = cv2.dnn.readNetFromDarknet('./models/yolov3.cfg', './models/yolov3.weights')
-net.setPreferableBackend(cv2.dnn.DNN_BACKEND_OPENCV)
-# determine the output layer
-ln = net.getLayerNames()
-print('layer: ', net.getUnconnectedOutLayers())
-ln = [ln[i - 1] for i in net.getUnconnectedOutLayers()]
+            self.min_width, self.min_height = 4, 4
+            # set model confidence level, at least 50% confident in model classification 
+            self.confidence_threshold = 0.5
+            self.nms_threshold = 0.25
+            # even though this is probably safe I don't feel great opening a pickle file from the web
+            #MODEL = pickle.load("model.p", "rb")
+
+            # Setting up yolo model instead
+            # Load names of classes and get random colors
+            self.classes = open('./models/coco.names').read().strip().split('\n')
+            np.random.seed(42)
+            self.colors = np.random.randint(0, 255, size=(len(self.classes), 3), dtype='uint8')
+
+            # Give the configuration and weight files for the model and load the network.
+            self.net = cv2.dnn.readNetFromDarknet('./models/yolov3.cfg', './models/yolov3.weights')
+            self.net.setPreferableBackend(cv2.dnn.DNN_BACKEND_OPENCV)
+            # determine the output layer
+            ln = self.net.getLayerNames()
+            print('layer: ', self.net.getUnconnectedOutLayers())
+            self.ln = [ln[i - 1] for i in self.net.getUnconnectedOutLayers()]
 
 
-def empty_or_not(spot_bgr):
+    def empty_or_not(self, spot_bgr):
 
-    obj_test = '' 
-  
-    spot_bgr = spot_bgr.astype(np.float32)
-    blob = cv2.dnn.blobFromImage(spot_bgr, 1/255.0, (416, 416), swapRB=True, crop=False)
+        obj_test = '' 
+        test_title = ''
+        spot_bgr = spot_bgr.astype(np.float32)
+        blob = cv2.dnn.blobFromImage(spot_bgr, 1/255.0, (416, 416), swapRB=True, crop=False)
 
-    #r = blob[0, 0, :, :]
-    net.setInput(blob)
-    t0 = time.time()
-    outputs = net.forward(ln)
-    t = time.time()
-    # print(len(outputs))
-    # for out in outputs:
-    #     print(out.shape)
+        #r = blob[0, 0, :, :]
+        self.net.setInput(blob)
+        t0 = time.time()
+        outputs = self.net.forward(self.ln)
+        t = time.time()
+        # print(len(outputs))
+        # for out in outputs:
+        #     print(out.shape)
 
-    # TODO: fine tune and refine
-    boxes = []
-    confidences = []
-    classIDs = []
-    h, w = spot_bgr.shape[:2]
-    for output in outputs:
-        for detection in output:
-            scores = detection[5:]
-            classID = np.argmax(scores)
-            confidence = scores[classID]
-            if confidence > confidence_threshold:
-                box = detection[:4] * np.array([w, h, w, h])
-                (centerX, centerY, width, height) = box.astype("int")
-                x = int(centerX - (width / 2))
-                y = int(centerY - (height / 2))
-                box = [x, y, int(width), int(height)]
-                boxes.append(box)
-                confidences.append(float(confidence))
-                classIDs.append(classID)
-    indices = cv2.dnn.NMSBoxes(boxes, confidences, confidence_threshold, nms_threshold)
-    # taking this from yolo example and only looking for classes detected
-    if len(indices) > 0:
-        for i in indices.flatten():
-            (x, y) = (boxes[i][0], boxes[i][1])
-            (w, h) = (boxes[i][2], boxes[i][3])
-            obj_test = classes[classIDs[i]]
-            text = "{}: {:.4f}".format(classes[classIDs[i]], confidences[i])
-            print(text)
+        # TODO: fine tune and refine
+        boxes = []
+        confidences = []
+        classIDs = []
+        h, w = spot_bgr.shape[:2]
+        for output in outputs:
+            for detection in output:
+                scores = detection[5:]
+                classID = np.argmax(scores)
+                confidence = scores[classID]
+                if confidence > self.confidence_threshold:
+                    box = detection[:4] * np.array([w, h, w, h])
+                    (centerX, centerY, width, height) = box.astype("int")
+                    x = int(centerX - (width / 2))
+                    y = int(centerY - (height / 2))
+                    box = [x, y, int(width), int(height)]
+                    boxes.append(box)
+                    confidences.append(float(confidence))
+                    classIDs.append(classID)
+        indices = cv2.dnn.NMSBoxes(boxes, confidences, self.confidence_threshold, self.nms_threshold)
+        # taking this from yolo example and only looking for classes detected
+        if len(indices) > 0:
+            for i in indices.flatten():
+                (x, y) = (boxes[i][0], boxes[i][1])
+                (w, h) = (boxes[i][2], boxes[i][3])
+                obj_test = self.classes[classIDs[i]]
+                text = "{}: {:.4f}".format(self.classes[classIDs[i]], confidences[i])
+                color = [int(c) for c in self.colors[classIDs[i]]]
+                test_title = text
+                #cv2.putText(lot_cv2_img, obj_test, (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 1)
+                print(text)
+        # return what object prediction is to display 
+        if obj_test == 'car' or obj_test == 'truck':
+            return self.NOT_EMPTY, test_title
+        else:
+            return self.EMPTY, test_title
+        
+    def get_parking_spots_bboxes(self, connected_components):
+        (totalLabels, label_ids, values, centroid) = connected_components
 
-    if obj_test == 'car' or obj_test == 'truck':
-        return NOT_EMPTY
-    else:
-        return EMPTY
-    
-def get_parking_spots_bboxes(connected_components):
-    (totalLabels, label_ids, values, centroid) = connected_components
-
-    slots = []
-    coef = 1
-    for i in range(1, totalLabels):
-        # get coords
-        x1 = int(values[i, cv2.CC_STAT_LEFT] * coef)
-        y1 = int(values[i, cv2.CC_STAT_TOP] * coef)
-        h = int(values[i, cv2.CC_STAT_HEIGHT] * coef)
-        w = int(values[i, cv2.CC_STAT_WIDTH] * coef)
-        # for some reason get some bounding boxes that are 1 to 4 pixels wide, check the width to skip adding them
-        if w < min_width or h < min_height:
-            continue       
-        slots.append([x1, y1, w, h])
-    return slots
+        slots = []
+        coef = 1
+        for i in range(1, totalLabels):
+            # get coords
+            x1 = int(values[i, cv2.CC_STAT_LEFT] * coef)
+            y1 = int(values[i, cv2.CC_STAT_TOP] * coef)
+            h = int(values[i, cv2.CC_STAT_HEIGHT] * coef)
+            w = int(values[i, cv2.CC_STAT_WIDTH] * coef)
+            # for some reason get some bounding boxes that are 1 to 4 pixels wide, check the width to skip adding them
+            if w < self.min_width or h < self.min_height:
+                continue       
+            slots.append([x1, y1, w, h])
+        return slots
